f609fe7f6002b89f8a1053acb336685159f5fa63
asmm735 <asmita735pal@gmail.com>
2026-01-28 16:08:21 +0530

    Hardcoded Google OAuth endpoints to fix userinfo error

--- api/index.py
+++ api/index.py
@@ -69,10 +69,12 @@ google = oauth.register(
     name='google',
     client_id=os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+    access_token_url='https://oauth2.googleapis.com/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',
+    client_kwargs={'scope': 'openid email profile'},
+    jwks_uri='https://www.googleapis.com/oauth2/v3/certs'
 )
 
 # --- MOCK DATA ---
