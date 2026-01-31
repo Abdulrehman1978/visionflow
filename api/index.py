95100d0d4fd92170c78a066c981a46a0017b7ef6
asmm735 <asmita735pal@gmail.com>
2026-01-31 12:23:42 +0530

    Fix db SSL connection and requirements

--- api/index.py
+++ api/index.py
@@ -55,6 +55,7 @@ else:
 
 app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {"pool_pre_ping": True}
 db = SQLAlchemy(app)
 
 # --- MODELS ---
